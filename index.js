826ba89f33e2d67a50ae3a72f62db839cf20a82c
Saurabh Rayakwar <saurabh.r@stylabs.in>
2017-12-19 13:20:33 +0530

    directory setup

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
+require('./services/passport');
+
 const app = express();
 
+require('./routes/authRoutes')(app);
 
 app.get('/', (req, res) => {
     res.send({'hello': 'there'});
 });
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 app.listen(PORT);
